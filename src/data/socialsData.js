1d161416fa0cc23cd9b852462a0a9884050ec71e
Tina Pham <tina.pham8912@gmail.com>
2022-03-08 19:47:21 -0800

    more updates

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,4 +1,5 @@
 export const socialsData = {
     github: 'https://github.com/tpham912',
-    linkedIn: 'https://www.linkedin.com/in/tinacpham/'
+    linkedIn: 'https://www.linkedin.com/in/tinacpham/',
+    instagram: 'https://www.instagram.com/tinasbeta/?hl=en'
 }
