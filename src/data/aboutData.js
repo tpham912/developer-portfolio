509c6de9697f856a5f8f84d747c4fff37fef3749
Tina Pham <tina.pham8912@gmail.com>
2022-03-08 18:39:30 -0800

    edited aboutme

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,9 @@
 export const aboutData = {
     title: "About Me",
-    description1: "My name's Tina. I'm a web developer based in Seattle, WA.",
-    description2: 'I was born and raised in Vietnam for the first 14 years of my life. I moved to the States before starting high-school in Maryland.  After, I attended the University of Texas at Dallas where I earned my Bachelor’s degree in General Biology. Currently, I reside in Seattle, Washington. I’ve worked in the healthcare field for almost 5 years and started my career in tech in July 2021. The technologies I’ve experienced with include HTML, CSS, JS, SQL with MySQL and I’ve also experienced with various JS frameworks such as React, Node, Express. I love hearing about new opportunities. Please feel free to reach out to me!',
+    description1: "My name's Tina Pham. I'm a web developer based in Seattle, WA.",
+    description2: 'I was born and raised in Vietnam for the first 14 years of my life. I moved to the States before starting high-school in Maryland. I attended the University of Texas at Dallas where I earned my Bachelor’s degree in General Biology. I’ve worked in the healthcare field for almost 5 years and started my career in tech in July 2021. The technologies I’ve experienced with include HTML, CSS, JS, SQL with MySQL and I’ve also experienced with various JS frameworks such as React, Node, Express.',
+    description3: 'When I’m away from the keyboard, I can be found at a climbing gym or in the mountain snowboarding. I also love to travel to meet and explore different cultures. ',
+    description4: 'This site is a collection of my skills, background, experiences and resources that have helped on this journey. Please feel free to reach out and thanks for stopping by!',
     image: 2
 }
+
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import jpg from '../assets/jpg/pp.jpg'
 export const headerData = {
     name: 'Tina Pham',
     title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.” - Bill Gates ",
     image: jpg,
     resumePdf: resume
 }
